e01a0523a571283803977893377b8f158cc618ad
Vladislav <vladislav.dolgov2023@mail.ru>
2026-03-12 19:52:05 +0700

    Добавлен тест функции csvToJSON, проверяющий работу функции с корректными входными данными

--- functins.test.ts
+++ functins.test.ts
@@ -2,3 +2,9 @@ import { describe, it, expect } from "vitest";
 import { readFile, writeFile } from 'node:fs/promises';
 
 import {csvToJSON, formatCSVFileToJSONFile} from './functions.ts'
+
+describe("Тесты функции csvToJSON", () => {
+    it("Вызов функции с корректными данными", () => {
+        expect(csvToJSON(["p1;p2", "1;A", "2;B"], ";")).toEqual([{"p1": 1, "p2": "A"}, {"p1": 2, "p2": "B"}]);
+    })
+});
