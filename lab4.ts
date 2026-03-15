6e0fc5dfbac5ae1387fa323670bd957ee7c3a7b0
Vladislav <vladislav.dolgov2023@mail.ru>
2026-03-15 12:06:05 +0700

    Описан тип Group<T>

--- lab4.ts
+++ lab4.ts
@@ -1,3 +1,10 @@
 type Transform<T> = (items: T[]) => T[];
 
 type Where<T> = <K extends keyof T>(key: K, value: T[K]) => Transform<T>;
+
+type Sort<T> = <K extends keyof T>(key: K) => Transform<T>;
+
+type Group<T, K extends keyof T> = {
+  key: T[K];
+  items: T[];
+};
