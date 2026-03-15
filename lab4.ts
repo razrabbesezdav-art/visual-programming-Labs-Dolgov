e185ad570e70194218c289e520cd1a50f0da9da9
Vladislav <vladislav.dolgov2023@mail.ru>
2026-03-15 12:46:58 +0700

    Описан тип GroupBy<T>, добавлены функции where, sort, groupBy, having

--- lab4.ts
+++ lab4.ts
@@ -9,8 +9,50 @@ type Group<T, K extends keyof T> = {
   items: T[];
 };
 
+type GroupBy<T> = <K extends keyof T>(key: K) => (items: T[]) => Group<T, K>[];
+
 type GroupTransform<T, K extends keyof T> = (groups: Group<T, K>[]) => Group<T, K>[];
 
 type Having<T> = <K extends keyof T>(
   predicate: (group: Group<T, K>) => boolean
 ) => GroupTransform<T, K>;
+
+type ID_card ={
+    id: number;
+    name: string;
+    address: string;
+    age: number;
+    phoneNumber: string;
+}
+const where: Where<ID_card> =
+    (key, value) =>
+    (data) =>
+        data.filter((item) => item[key] === value);
+
+const sort: Sort<ID_card> =
+    (key) =>
+    (data) =>
+        [...data].sort((a, b) => {
+            const av = a[key];
+            const bv = b[key];
+            if (av < bv) return -1;
+            if (av > bv) return 1;
+            return 0;
+        });
+
+const groupBy: GroupBy<ID_card> =
+    (key) =>
+    (data) =>
+        Object.values(
+            data.reduce((acc, item) => {
+                const k = item[key] as unknown as string;
+                (acc[k] ??= { key: item[key], items: [] }).items.push(item);
+                return acc;
+            }, {} as Record<string, Group<ID_card, typeof key>>),
+        );
+
+const having: Having<ID_card> =
+ (predicate) =>
+ (groups) =>
+ groups.filter(predicate);
+
